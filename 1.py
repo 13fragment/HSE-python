cde83701a294c2fba95e92db8d63763267be80c0
4kith <mgolubevbiz@gmail.com>
2022-09-30 15:22:20 +0300

    update content

---
+++ 1.py
@@ -0,0 +1,8 @@
+name = 'Maxim'
+surname = 'Golubev'
+birthDate = 1999
+print(name,surname,birthDate)
+transitVariable = name
+name = surname
+surname = transitVariable
+print(name,surname,birthDate+60)
