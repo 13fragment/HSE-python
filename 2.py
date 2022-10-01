f1fd37a0fd67d8abf90550b8a5966b9a2e30c2bb
4kith <mgolubevbiz@gmail.com>
2022-10-01 19:59:47 +0300

    update solution by using ternary operation

--- 2.py
+++ 2.py
@@ -1,8 +1,5 @@
 katet1,katet2,gypotenuza = float(input('Катет 1 = ')),float(input('Катет 2 = ')) ,float(input('Гипотенуза = ')) 
 square = 0
-if katet1**2 + katet2**2 == gypotenuza**2:
-    square = katet1*katet2/2
-    print(square)
-else:
-    print('Ошибка, данный треугольник не является прямоугольным')
+square = print('Площадь треугольника = ',katet1*katet2/2) if katet1**2 + katet2**2 == gypotenuza**2 else print('Ошибка, данный треугольник не является прямоугольным')
+
 
