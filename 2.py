f53b41f4469460aa4729803a66f85864e0976c32
4kith <mgolubevbiz@gmail.com>
2022-10-02 00:07:01 +0300

    unuseful param

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 katet1,katet2,gypotenuza = float(input('Катет 1 = ')),float(input('Катет 2 = ')) ,float(input('Гипотенуза = ')) 
-square = print('Площадь треугольника = ',katet1*katet2/2) if katet1**2 + katet2**2 == gypotenuza**2 else print('Ошибка, данный треугольник не является прямоугольным')
+print('Площадь треугольника = ',katet1*katet2/2) if katet1**2 + katet2**2 == gypotenuza**2 else print('Ошибка, данный треугольник не является прямоугольным')
 # при желании можно вообще в одну строчку завернуть, но тогда код вообще плохо читаемый становится)
 # katet1,katet2,gypotenuza = float(input('Катет 1 = ')),float(input('Катет 2 = ')) ,float(input('Гипотенуза = ')) ;square = print('Площадь треугольника = ',katet1*katet2/2) if katet1**2 + katet2**2 == gypotenuza**2 else print('Ошибка, данный треугольник не является прямоугольным')
 
