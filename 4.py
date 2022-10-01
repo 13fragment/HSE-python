ae15945d7e1c6d04d2a076ea3171b5d0e040f8fa
4kith <mgolubevbiz@gmail.com>
2022-10-01 22:56:56 +0300

    update solution

--- 4.py
+++ 4.py
@@ -1,7 +1,6 @@
 # взял в среднем что в году 365 дней
 uslovie = 8*60/5 #исходя из условия столько человек в среднем может посмотреть экспонатов за 1 день
 kolvoExp,kolvoVremeni = int(input('Количество экспонатов = ')), int(input('Количество лет = ' ))
-k = 0
 # if kolvoVremeni | kolvoExp <=0:
 #     print('Ошибка, введены неверные значения') можно сделать такой вариант более короткий но не более явный для пользователя
 # а можно сделать так
@@ -12,10 +11,7 @@ elif kolvoVremeni < 0:
 elif kolvoVremeni==kolvoExp==0:
     print('Ошибка, введены неверные значение экспонатов и времени')
 if kolvoExp > 0 & kolvoVremeni >=0:
-    k = kolvoExp / uslovie
-    print('Понадобится времени что осмотреть все экспонаты в днях = ',k)
+    print('Понадобится времени что осмотреть все экспонаты в днях = ',kolvoExp / uslovie)
 if kolvoVremeni > 0 & kolvoExp>=0:
-    kolvoVremeni = kolvoVremeni*365*24
-    k = kolvoVremeni * uslovie
-    print('Кол-во экспонатов, осмотренных по заданому времени равно = ',k)
+    print('Кол-во экспонатов, осмотренных по заданому времени равно = ',kolvoVremeni*365*24*uslovie )
 
