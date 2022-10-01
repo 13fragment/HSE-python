43c35a4f24d94729d8db8e43ebe0b0d690d0ea53
4kith <mgolubevbiz@gmail.com>
2022-10-01 20:16:05 +0300

    deleate useless string

--- 2.py
+++ 2.py
@@ -1,5 +1,4 @@
 katet1,katet2,gypotenuza = float(input('Катет 1 = ')),float(input('Катет 2 = ')) ,float(input('Гипотенуза = ')) 
-square = 0
 square = print('Площадь треугольника = ',katet1*katet2/2) if katet1**2 + katet2**2 == gypotenuza**2 else print('Ошибка, данный треугольник не является прямоугольным')
 
 
