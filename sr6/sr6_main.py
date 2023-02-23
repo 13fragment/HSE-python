1855ace792b77974f3e15f05da563c34dd474996
Max <74821169+13fragment@users.noreply.github.com>
2023-02-24 02:11:37 +0300

    check grammar

--- sr6/sr6_main.py
+++ sr6/sr6_main.py
@@ -7,7 +7,7 @@ vvod()
 if (n > 0 or m>0):
     print('Ошибка')
 def prints():
-    print('Нет совподающих элементов')
+    print('Нет совпадающих элементов')
 for i in range(n):
     a[i] = input('Элемент 1 массива = ')
 for l in range(m):
