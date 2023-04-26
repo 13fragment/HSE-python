4b6332bedfbc79d974d480d915f5d7ce4a95a8df
13fragment <mgolubevbiz@gmail.com>
2023-04-26 07:02:19 +0300

    solve 2 task

--- 21.04.23/generators.py
+++ 21.04.23/generators.py
@@ -3,3 +3,9 @@
 #     return (f"   * \n  *** \n *****\n******* \n")*n
 # print(xmasTree())
 
+n,m = map(int,input().split())
+a = [[(int)(j-i+1)%2 for i in range(m)] for j in range(n)]
+for i in range(n):
+    for j in range(m):
+        print(a[i][j],end=" ")
+    print()
