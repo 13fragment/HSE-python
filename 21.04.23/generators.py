084664db11d7d46eabab341d020cc2619997b912
13fragment <mgolubevbiz@gmail.com>
2023-04-26 07:04:57 +0300

    solve 2 task

--- 21.04.23/generators.py
+++ 21.04.23/generators.py
@@ -3,6 +3,13 @@
 #     return (f"   * \n  *** \n *****\n******* \n")*n
 # print(xmasTree())
 
+# n,m = map(int,input().split())
+# a = [[(int)(j-i+1)%2 for i in range(m)] for j in range(n)]
+# for i in range(n):
+#     for j in range(m):
+#         print(a[i][j],end=" ")
+#     print()
+
 n,m = map(int,input().split())
 a = [[(int)(j-i+1)%2 for i in range(m)] for j in range(n)]
 for i in range(n):
