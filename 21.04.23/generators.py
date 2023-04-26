dc204098d5335c3b29c9d561330cf45a1043d2b9
13fragment <mgolubevbiz@gmail.com>
2023-04-26 08:21:18 +0300

    solve 3 task

--- 21.04.23/generators.py
+++ 21.04.23/generators.py
@@ -10,9 +10,10 @@
 #         print(a[i][j],end=" ")
 #     print()
 
-n,m = map(int,input().split())
-a = [[(int)(j-i+1)%2 for i in range(m)] for j in range(n)]
-for i in range(n):
-    for j in range(m):
-        print(a[i][j],end=" ")
-    print()
+# n,m = map(int,input().split())
+# f = int()
+# a = [[i*4+j+1 for j in range(m)] for i in range(n)]
+# for i in range(n):
+#     for j in range(m):
+#         print(a[i][j],end=" ")
+#     print()
