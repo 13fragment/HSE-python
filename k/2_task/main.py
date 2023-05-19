477dc9f04256a4c0d4cc8ba39653009f1ed68cfb
13fragment <mgolubevbiz@gmail.com>
2023-05-19 14:18:15 +0300

    update 2 task

--- k/2_task/main.py
+++ k/2_task/main.py
@@ -2,20 +2,16 @@ import os
 import glob
 from collections import Counter
 
-directory = "/home/max/PycharmProjects/HSE-python/k/2_task" # заменить на нужный путь
+directory = "/home/max/PycharmProjects/HSE-python/k/2_task"
 
-# создаем список всех файлов *.txt в заданном каталоге
 files_list = glob.glob(os.path.join(directory, '*.txt'))
 
-# проверяем, что в списке не менее 3 файлов
 if len(files_list) < 3:
     print('В заданном каталоге недостаточно файлов')
     exit()
 
-# создаем пустой список, куда будем добавлять все слова из файлов
 all_words = []
 
-# проходим по каждому файлу и добавляем все слова в список all_words
 for file in files_list:
     with open(file, 'r') as f:
         lines = f.readlines()
@@ -23,7 +19,6 @@ for file in files_list:
             words = line.strip().split()
             all_words.extend(words)
 
-# находим наиболее часто встречающееся слово
 word_counts = Counter(all_words)
 most_common_word = word_counts.most_common(1)[0][0]
 
